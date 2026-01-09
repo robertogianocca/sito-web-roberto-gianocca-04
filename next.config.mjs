56b3f6673fba02c017b95890ba2c8b2b0e774271
Roberto <robertogianocca@outlook.com>
2026-01-09 21:17:28 +0100

    feat: add homepage redirect to video

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  /* custum config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/video",
+        permanent: false, // Use false for a temporary redirect (307 status code)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
